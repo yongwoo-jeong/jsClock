e8b84489bced74ae4d79989549c85f54d9467ce7
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-05-29 19:25:07 +0900

    3-4

---
+++ todo.js
@@ -0,0 +1,30 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = form.querySelector("input"),
+  toDOList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos'
+
+function paintToDo(text) {
+
+}
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+}
+
+function loadToDos() {
+    const toDos = localStorage.getItem(TODOS_LS);
+    if(toDos !=== null) {
+
+    } else {
+
+    }
+}
+
+  function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+  }
+
+  init();
