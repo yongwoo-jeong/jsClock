aa4ecd3621f8cd1f0ac4a51628ee28bc0f281575
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-07-16 23:16:48 +0900

    before quote prepend

---
+++ js/quote.js
@@ -0,0 +1,17 @@
+const quote = document.querySelector(".quote");
+
+const quoteNumber = 5;
+
+function() showQuote() {
+    const quote = 
+}
+
+function getRandom() {
+  const number = Math.floor(Math.random() * quoteNumber);
+  return number;
+}
+
+function init() {
+  const randomNumber = getRandom();
+  showQuote(randomNumber)
+}
