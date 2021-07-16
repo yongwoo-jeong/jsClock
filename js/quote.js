27aa38ba728d32cc7ff1994ae2aec99b140f8b38
yongwoo-jeong <80328674+yongwoo-jeong@users.noreply.github.com>
2021-07-16 23:33:10 +0900

    gotta modify qoute css

--- js/quote.js
+++ js/quote.js
@@ -1,11 +1,17 @@
-const quote = document.querySelector(".quote");
+const quoteDiv = document.querySelector(".js-quoteDiv");
 
 const quoteNumber = 5;
 
-function() showQuote() {
-    const quote = 
+function showQuote(number) {
+  quote = [
+    "The greatest glory in living lies not in never falling,\nbut in rising every time we fall. \n-Nelson Mandela",
+    "The way to get started is to quit talking and begin doing. \n-Walt Disney",
+    "Your time is limited, so don't waste it living someone else's life. \nDon't be trapped by dogma – which is living with the results of other people's thinking.\n-Steve Jobs",
+    "If life were predictable it would cease to be life, and be without flavor. \n-Eleanor Roosevelt",
+    "If you look at what you have in life, you'll always have more.\nIf you look at what you don't have in life, you'll never have enough. \n-Oprah Winfrey",
+  ];
+  quoteDiv.textContent = quote[number];
 }
-
 function getRandom() {
   const number = Math.floor(Math.random() * quoteNumber);
   return number;
@@ -13,5 +19,6 @@ function getRandom() {
 
 function init() {
   const randomNumber = getRandom();
-  showQuote(randomNumber)
+  showQuote(randomNumber);
 }
+init();
